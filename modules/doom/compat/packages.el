7ef14e58c4e40f55e8ebe0e717af50a84074dda8
Henrik Lissner <git@henrik.io>
2026-06-11 02:22:13 -0400

    fix(compat): gate package behind +use-package
    Amend: b4c55ccc5f7c

--- modules/doom/compat/packages.el
+++ modules/doom/compat/packages.el
@@ -4,13 +4,14 @@
 ;; TODO: Mirror these packages on doomelpa, to "immortalize" this module and
 ;;   make Doom core resistant to upstream breakage.
 
-(unless (fboundp 'lisp-data-mode)
-  (package! bind-key
-    ;; HACK: bind-key-pkg.el tries to set the mode to lisp-data-mode, which
-    ;;   doesn't exist prior to Emacs 28.x, so bind-key will fail to build.
-    :recipe (:files ("bind-key.el"))
-    :pin "6ff8788e347ce31b5c3c4647c2e22e7ee2c5ab7c"))
-(package! use-package :pin "4b3484b550431f74ab9cda060a8dc983fe482131")
+(when (modulep! +use-package)
+  (unless (fboundp 'lisp-data-mode)
+    (package! bind-key
+      ;; HACK: bind-key-pkg.el tries to set the mode to lisp-data-mode, which
+      ;;   doesn't exist prior to Emacs 28.x, so bind-key will fail to build.
+      :recipe (:files ("bind-key.el"))
+      :pin "6ff8788e347ce31b5c3c4647c2e22e7ee2c5ab7c"))
+  (package! use-package :pin "4b3484b550431f74ab9cda060a8dc983fe482131"))
 
 (when (modulep! +smartparens)
   (package! smartparens :pin "82d2cf084a19b0c2c3812e0550721f8a61996056"))
