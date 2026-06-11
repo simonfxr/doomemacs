7635aabb758e3a002a3a7de16b2c9f7bc2bf88ee
Henrik Lissner <git@henrik.io>
2026-06-11 05:16:55 -0400

    fix(:doom): unquoted package-archives ref w/ add-to-list
    Whoops. Seriously. What am I doing pushing code at 5am?
    
    Fix: #8813
    Amend: 6d557514413a

--- modules/doom/init.el
+++ modules/doom/init.el
@@ -258,8 +258,8 @@ Otherwise, `en/disable-command' (in novice.el.gz) is hardcoded to write them to
   (let ((s (if (gnutls-available-p) "s" "")))
     ;; I omit Marmalade because its packages are manually submitted rather than
     ;; pulled, and so often out of date.
-    (add-to-list package-archives `("melpa" . ,(format "http%s://melpa.org/packages/" s)))
-    (add-to-list package-archives `("org"   . ,(format "http%s://orgmode.org/elpa/"   s))))
+    (add-to-list 'package-archives `("melpa" . ,(format "http%s://melpa.org/packages/" s)))
+    (add-to-list 'package-archives `("org"   . ,(format "http%s://orgmode.org/elpa/"   s))))
   ;; Refresh package.el the first time you call `package-install', so it's still
   ;; trivially usable. Remember to run 'doom sync' to purge them; they can
   ;; conflict with packages installed via straight!
