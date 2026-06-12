81360ffcfdde7cf75f16b6310af9fe7fa2d5223d
Henrik Lissner <git@henrik.io>
2026-06-12 18:53:04 -0400

    refactor(compat): move :config default +smartparens here

--- modules/doom/compat/packages.el
+++ modules/doom/compat/packages.el
@@ -18,7 +18,6 @@
 
 ;; (package! auto-minor-mode :pin "17cfa1b54800fdef2975c0c0531dad34846a5065")
 ;; (package! better-jumper :pin "b1bf7a3c8cb820d942a0305e0e6412ef369f819c")
-;; (package! smartparens :pin "82d2cf084a19b0c2c3812e0550721f8a61996056")
 ;; (package! projectile :pin "6449fb0465587c757c1fbf1a64bb4ba420a78bfa")
 ;; (package! general :pin "a48768f85a655fe77b5f45c2880b420da1b1b9c3")
 ;; (package! which-key :pin "38d4308d1143b61e4004b6e7a940686784e51500")
