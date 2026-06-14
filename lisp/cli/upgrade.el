simonfxr/doomemacs

commit 4bf6055380eacfd5dfe2d2b85b9b3065509fa22a

fix(cli): update submodules in doom {upgrade,install}
In preparation for modules to be moved out of core.

lisp/cli/upgrade.el
@@ -36,8 +36,8 @@ libraries. It is the equivalent of the following shell commands:
     (cond
      (packages?
       (or (zerop (car (sh! "git" "-C" doom-emacs-dir
-                           "submodule" "update" "--init" "--recursive")))
-          (error "Failed to update submodules"))
+                           "submodule" "update" "-f" "--init" "--recursive")))
+          (error "Failed to update submodules. Run `doom upgrade -p` again"))
 
       ;; HACK: It's messy to use straight to upgrade straight, due to the
       ;;   potential for backwards incompatibility, so we staticly check if
@@ -50,11 +50,11 @@ libraries. It is the equivalent of the following shell commands:
           (print! (item "Preparing straight for an update"))
           (delete-directory (doom-path straight-base-dir "straight/repos/straight.el")
                             'recursive)))
-      (call! (append '("sync" "-u")
-                     (if aot? '("--aot"))
-                     (if nobuild? '("-B"))
-                     (if jobs `("-j" ,jobs))))
-      (print! (success "Finished upgrading Doom Emacs")))
+
+      (exit! "doom" "sync" "-u"
+             (if aot? '("--aot"))
+             (if nobuild? '("-B"))
+             (if jobs `("-j" ,jobs))))
 
      ((doom-cli-upgrade context force? force?)
       ;; Reload Doom's CLI & libraries, in case there were any upstream changes.

lisp/cli/install.el
@@ -95,19 +95,6 @@ Change `$DOOMDIR' with the `--doomdir' option, e.g.
       (after! straight
         (setq straight--native-comp-available t)))
 
-    ;; Install Doom packages
-    (if (eq install? :no)
-        (print! (warn "Not installing plugins, as requested"))
-      (print! (start "Installing plugins"))
-      (print-group! (doom-packages-ensure)))
-
-    (when (doom-profiles-bootloadable-p)
-      (print! (start "Initializing profile bootstrapper..."))
-      (call! '(profile sync "--all" "--reload")))
-
-    (print! (start "Synchronizing default profile..."))
-    (print-group! (doom-profile-generate))
-
     (if (eq hooks? :no)
         (print! (warn "Not deploying commit-msg and pre-push git hooks, as requested"))
       (print! (start "Deploying commit-msg and pre-push git hooks"))
@@ -117,13 +104,16 @@ Change `$DOOMDIR' with the `--doomdir' option, e.g.
           ('user-error
            (print! (warn "%s") (error-message-string e))))))
 
-    (when (file-exists-p "~/.emacs")
-      (print! (warn "A ~/.emacs file was detected. This conflicts with Doom and should be deleted!")))
+    (if (eq install? :no)
+        (print! (warn "Not installing plugins, as requested"))
+      (when (file-exists-p "~/.emacs")
+        (print! (warn "~/.emacs file detected. Delete it or Emacs won't load Doom correctly!")))
 
-    (print! (success "Finished! Doom is ready to go!\n"))
-    (with-temp-buffer
-      (insert-file-contents (doom-path doom-emacs-dir "static/QUICKSTART_INTRO"))
-      (print! "%s" (buffer-string)))))
+      ;; Ensure module sources are initialized
+      (or (zerop (car (sh! "git" "submodule" "update" "-f" "--init" "--recursive")))
+          (error "Failed to update submodules"))
+      (doom-cli-context-put context 'installing t)
+      (exit! "doom" "sync" (if aot? "--aot")))))
 
 (provide 'doom-cli-install)
 ;;; install.el ends here

lisp/cli/sync.el
@@ -116,6 +116,13 @@ OPTIONS:
             (with-temp-file doom-sync-info-file
               (prin1 (cons emacs-version (doom-sync--system-hash))
                      (current-buffer))))
+          (cond ((doom-cli-context-get context 'installing)
+                 (print! (success "Doom successfully installed!"))
+                 (with-temp-buffer
+                   (insert-file-contents (doom-path doom-emacs-dir "static/QUICKSTART_INTRO"))
+                   (print! "%s" (buffer-string))))
+                ((doom-cli-context-get context 'upgrading)
+                 (print! (success "Doom successfully upgraded!"))))
           t)
       (remove-hook 'kill-emacs-hook #'doom-sync--abort-warning-h))))
 

commit 8a3dc4abe2f293700275dde3f90b469538404e8a

fix(cli): doom upgrade -p: stringify --jobs argument
A regression introduced in 4bf6055. JOBS contains an integer and `exit!`
expects all arguments to be strings (or nil).

Amend: 4bf6055380ea
Fix: #8818
Close: #8819
Co-authored-by: xz-dev <xz-dev@users.noreply.github.com>

lisp/cli/upgrade.el
@@ -55,7 +55,7 @@ libraries. It is the equivalent of the following shell commands:
       (exit! "doom" "sync" "-u"
              (if aot? '("--aot"))
              (if nobuild? '("-B"))
-             (if jobs `("-j" ,jobs))))
+             (if jobs (format "--jobs=%d" jobs))))
 
      ((doom-cli-upgrade context force? force?)
       ;; Reload Doom's CLI & libraries, in case there were any upstream changes.