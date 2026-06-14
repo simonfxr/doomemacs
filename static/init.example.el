975485a908e37e2b68756a7ce1c9857459ec9821
Henrik Lissner <git@henrik.io>
2026-06-14 01:10:44 -0400

    docs: init.example.el: add :lang scad
    Ref: doomemacs/modules#4

--- static/init.example.el
+++ static/init.example.el
@@ -171,6 +171,7 @@
        ;;rst               ; ReST in peace
        ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        ;;(rust +lsp)       ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       ;;scad              ; trust the preview, regret the render
        ;;scala             ; java, but good
        ;;(scheme +guile)   ; a fully conniving family of lisps
        sh                ; she sells {ba,z,fi}sh shells on the C xor
