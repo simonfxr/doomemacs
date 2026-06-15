simonfxr/doomemacs

commit ff0a83665321d92a13e8b1b4fe5ab46b189d61ae

feat(lib): add directory helpers
Backports path constructor functions from v3 for each of Doom's
doom-*-dir variables to ensure forwards compatibility in core and
modules.

Care was taken to also ensure backwards compatibility and avoid changing
`doom-local-dir` (though it *will* happen in v3), and so their
implementations are a little hacky. That's okay, for now.

lisp/doom-cli.el
@@ -162,7 +162,7 @@ Only applies if (exit! :pager) or (exit! :pager?) are called."
   :group 'doom-cli)
 
 ;;; Logger settings
-(defvar doom-cli-log-file-format (expand-file-name "logs/cli.%s.%s.%s" doom-state-dir)
+(defvar doom-cli-log-file-format (doom-state-dir "logs/cli.%s.%s.%s")
   "Where to write any output/log file to.
 
 Must have two arguments, one for session id and the other for log type.")
@@ -1172,7 +1172,7 @@ Emacs' batch library lacks an implementation of the exec system call."
          (persisted-env
           (cl-remove-if-not
            #'cdr (append
-                  `(("DOOMPROFILE" . ,(ignore-errors (doom-profile->id doom-profile)))
+                  `(("DOOMPROFILE" . ,(doom-profile->id doom-profile))
                     ("EMACSDIR" . ,doom-emacs-dir)
                     ("DOOMDIR" . ,doom-user-dir)
                     ("DEBUG" . ,(if doom-debug-mode (number-to-string doom-log-level)))
@@ -1216,7 +1216,7 @@ Emacs' batch library lacks an implementation of the exec system call."
                           concat (format "%s=%s \\\n" var (shell-quote-argument val))
                           else do (doom-log 1 "restart: discarding envvar %S for being too long (%d)" var (length val)))
                 ,(format "PATH=\"%s%s$PATH\" \\\n"
-                         (doom-path doom-emacs-dir "bin")
+                         (doom-emacs-dir "bin")
                          path-separator)
                 "_doomrun \"$@\"\n"))
          (`pwsh `("try {\n"
@@ -2474,7 +2474,7 @@ OPTIONS:
     (doom/version)
     (terpri)
     (with-temp-buffer
-      (insert-file-contents (doom-path doom-emacs-dir "LICENSE"))
+      (insert-file-contents (doom-emacs-dir "LICENSE"))
       (re-search-forward "^Copyright (c) ")
       (print! "%s\n" (trim (thing-at-point 'line t)))
       (print! (p "Doom Emacs uses the MIT license and is provided without warranty "

lisp/cli/sync.el
@@ -15,7 +15,7 @@
   "Hooks run before 'doom sync' synchronizes the user's config with Doom.")
 
 ;; DEPRECATED: Will be removed once `doom-profile' is a struct
-(defvar doom-sync-info-file (file-name-concat doom-profile-data-dir "sync"))
+(defvar doom-sync-info-file (doom-profile-data-dir t "sync"))
 
 
 ;;
@@ -75,7 +75,7 @@ OPTIONS:
       (call! '(profile sync "--all")))
     (run-hooks 'doom-before-sync-hook)
 
-    (print! (start "Synchronizing %S profile..." ) (or (car doom-profile) "default"))
+    (print! (start "Synchronizing %S profile..." ) (car doom-profile))
     (unwind-protect
         (print-group!
           ;; If the user has up/downgraded Emacs since last sync, or copied their
@@ -119,7 +119,7 @@ OPTIONS:
           (cond ((doom-cli-context-get context 'installing)
                  (print! (success "Doom successfully installed!"))
                  (with-temp-buffer
-                   (insert-file-contents (doom-path doom-emacs-dir "static/QUICKSTART_INTRO"))
+                   (insert-file-contents (doom-emacs-dir "static/QUICKSTART_INTRO"))
                    (print! "%s" (buffer-string))))
                 ((doom-cli-context-get context 'upgrading)
                  (print! (success "Doom successfully upgraded!"))))

lisp/doom-emacs.el
@@ -268,7 +268,7 @@ multiple frames focused at once)."
 ;;; * File/Directory paths
 
 ;; User themes should live in $DOOMDIR/themes, not ~/.emacs.d
-(setq custom-theme-directory (file-name-concat doom-user-dir "themes/"))
+(setq custom-theme-directory (doom-user-dir "themes/"))
 
 ;; Third party themes add themselves to `custom-theme-load-path', but the themes
 ;; living in $DOOMDIR/themes should always have priority.
@@ -279,12 +279,12 @@ multiple frames focused at once)."
 ;; If a packages doesn't use `user-emacs-directory' or `locate-user-emacs-file'
 ;; to set their file/dir variables, then we need to set them ourselves to avoid
 ;; littering in ~/.emacs.d/.
-(setq desktop-dirname  (file-name-concat doom-profile-state-dir "desktop")
-      pcache-directory (file-name-concat doom-profile-cache-dir "pcache/"))
+(setq desktop-dirname  (doom-profile-state-dir t "desktop")
+      pcache-directory (doom-profile-cache-dir t "pcache/"))
 
 ;; Write custom.el settings to $DOOMDIR/custom.el instead of $EMACSDIR/init.el,
 ;; allowing users to version control them and not interfere with Doom init.
-(setq custom-file (file-name-concat doom-user-dir "custom.el"))
+(setq custom-file (doom-user-dir "custom.el"))
 
 (define-advice en/disable-command (:around (fn &rest args) write-to-data-dir)
   "Save safe-local-variables to `custom-file' instead of `user-init-file'.
@@ -463,7 +463,7 @@ Otherwise, `en/disable-command' (in novice.el.gz) is hardcoded to write them to
       delete-old-versions t ; clean up after itself
       kept-old-versions 5
       kept-new-versions 5
-      backup-directory-alist `(("." . ,(file-name-concat doom-profile-cache-dir "backup/")))
+      backup-directory-alist `(("." . ,(doom-profile-cache-dir t "backup/")))
       tramp-backup-directory-alist backup-directory-alist)
 
 ;; But turn on auto-save, so we have a fallback in case of crashes or lost data.
@@ -474,7 +474,7 @@ Otherwise, `en/disable-command' (in novice.el.gz) is hardcoded to write them to
       ;; just deleted, but I believe that's VCS's jurisdiction, not ours.
       auto-save-include-big-deletions t
       ;; Keep it out of `doom-emacs-dir' or the local directory.
-      auto-save-list-file-prefix (file-name-concat doom-profile-cache-dir "autosave/")
+      auto-save-list-file-prefix (doom-profile-cache-dir t "autosave/")
       ;; This resolves two issue while ensuring auto-save files are still
       ;; reasonably recognizable at a glance:
       ;;
@@ -1356,7 +1356,7 @@ triggering hooks during startup."
 
 
 ;;;###package bookmark
-(setq bookmark-default-file (file-name-concat doom-profile-data-dir "bookmarks"))
+(setq bookmark-default-file (doom-profile-data-dir t "bookmarks"))
 
 
 ;;;###package comint
@@ -1542,7 +1542,7 @@ triggering hooks during startup."
 
 
 ;;;###package project
-(setq project-list-file (file-name-concat doom-profile-state-dir "projects"))
+(setq project-list-file (doom-profile-state-dir t "projects"))
 (with-eval-after-load 'project
   ;; Not valid vc backends, but I use it to inform (global) file index
   ;; exclusions below and elsewhere.
@@ -1557,7 +1557,7 @@ triggering hooks during startup."
 ;;;###package recentf
 ;; Keep track of recently opened files
 (doom-load-packages-incrementally '(easymenu tree-widget timer recentf))
-(setq recentf-save-file (file-name-concat doom-profile-cache-dir "recentf"))
+(setq recentf-save-file (doom-profile-cache-dir t "recentf"))
 (add-hook 'doom-first-file-hook #'recentf-mode)
 (autoload 'recentf-open-files "recentf" nil t)
 (with-eval-after-load 'recentf
@@ -1604,7 +1604,7 @@ triggering hooks during startup."
 ;;;###package savehist
 ;; persist variables across sessions
 (doom-load-packages-incrementally '(custom))
-(setq savehist-file (file-name-concat doom-profile-cache-dir "savehist"))
+(setq savehist-file (doom-profile-cache-dir t "savehist"))
 (add-hook 'doom-first-input-hook #'savehist-mode)
 (with-eval-after-load 'savehist
   (setq savehist-save-minibuffer-history t
@@ -1638,7 +1638,7 @@ the unwritable tidbits."
 
 ;;;###package saveplace
 ;; persistent point location in buffers
-(setq save-place-file (file-name-concat doom-profile-cache-dir "saveplace"))
+(setq save-place-file (doom-profile-cache-dir t "saveplace"))
 (add-hook 'doom-first-input-hook #'savehist-mode)
 (with-eval-after-load 'savehist
   (defadvice! doom--recenter-on-load-saveplace-a (&rest _)

lisp/doom-lib.el
@@ -290,7 +290,6 @@ list is returned as-is."
 ;;
 ;;; Public library
 
-
 (defun doom-unquote (exp)
   "Return EXP unquoted."
   (declare (pure t) (side-effect-free t))
@@ -444,6 +443,71 @@ TRIGGER-HOOK is a list of quoted hooks and/or sharp-quoted functions."
           (advice-add 'after-find-file :before fn '((depth . -101)))
         (add-hook hook fn -101))
       fn)))
+
+
+;;
+;;; Directory helpers
+
+;; These are intentional facsimiles of their final implementations, meant solely
+;; for forward-compatibility with v3.
+
+(defsubst doom--profile (profile)
+  (let ((p (if (eq profile t) doom-profile profile)))
+    (unless (equal p doom--profile-default)
+      p)))
+
+(defsubst doom--dir (dir segments)
+  (let ((segments (delq nil segments))
+        file-name-handler-alist)
+    (if segments
+        (expand-file-name
+         (if (cdr segments)
+             (apply #'file-name-concat segments)
+           (car segments))
+         dir)
+      (expand-file-name dir))))
+
+(dolist (var '(doom-emacs-dir
+               doom-core-dir
+               doom-user-dir
+               doom-data-dir
+               doom-state-dir
+               doom-cache-dir))
+  (defalias var
+    (lambda (&rest segments)
+      (doom--dir (symbol-value var) segments))
+    (format "Return a path from SEGMENTS after `%s'." var)))
+
+(dolist (var '((doom-profile-data-dir  . doom-data-dir)
+               (doom-profile-cache-dir . doom-cache-dir)
+               (doom-profile-state-dir . doom-state-dir)))
+  (defalias (car var)
+    (lambda (profile &rest segments)
+      (doom--dir (file-name-concat
+                  (symbol-value (cdr var))
+                  (car (doom--profile profile)))
+                 segments))
+    (format "Return a local PROFILE path from SEGMENTS after `%s'.
+
+If PROFILE is t, default to the active profile."
+            (cdr var))))
+
+(defun doom-profile-dir (profile &rest segments)
+  "Return a path from SEGMENTS after a PROFILE's root data directory."
+  (doom--dir (file-name-concat doom-data-dir (car (doom--profile profile)))
+             segments))
+
+(defun doom-profile-init-dir (profile &rest segments)
+  "Return a path from SEGMENTS after a PROFILE's init files directory."
+  (apply #'doom-profile-dir profile "@" (cdr (doom--profile profile))
+         segments))
+
+(defun doom-profile-init-file (profile &optional filename)
+  "Return a path to a PROFILE's FILENAME (or its init.%d.%d.el file)."
+  (doom-profile-init-dir
+   profile (or filename (format "init.%d.%d.el"
+                                emacs-major-version
+                                emacs-minor-version))))
 
 
 ;;
@@ -1827,18 +1891,6 @@ If DEFAULT? is non-nil, an unspecified CAR/CDR will fall bakc to (_default .
           ((signal 'wrong-type-argument
                    (list "Expected PROFILE to be a string, cons cell, or `doom-profile'"
                          (type-of profile) profile))))))
-
-(defun doom-profile-init-file (profile)
-  "Return the init file for PROFILE."
-  (declare (side-effect-free t))
-  (cl-destructuring-bind (name . ref)
-      (if profile
-          (doom-profile-key profile t)
-        (cons nil nil))
-    (file-name-concat doom-data-dir name "@" ref
-                      (format "init.%d.%d.el"
-                              emacs-major-version
-                              emacs-minor-version))))
 
 (defun doom-profile-get (profile-name &optional property null-value)
   "Return PROFILE-NAME's PROFILE, otherwise its PROPERTY, otherwise NULL-VALUE."

lisp/doom.el
@@ -163,6 +163,10 @@
 (defvar doom-init-time nil
   "The time it took, in seconds (as a float), for Doom Emacs to start up.")
 
+;;; DEPRECATED: Will be removed in v3
+(defconst doom--noprofile nil)
+(defconst doom--profile-default (cons "_default" "0"))
+
 (defconst doom-profile
   (if-let* ((profile (getenv-internal "DOOMPROFILE")))
       (save-match-data
@@ -170,9 +174,8 @@
             (cons (match-string 1 profile)
                   (match-string 2 profile))
           (cons profile "0")))
-    ;; TODO: Restore this in v3
-    ;; (cons "_" "0")
-    )
+    (setq doom--noprofile t)
+    doom--profile-default)
   "The active profile as a cons cell (NAME . VERSION).")
 
 ;;; Data directory variables
@@ -227,12 +230,12 @@ These files should not be shared across systems. By default, it is used by
 `doom-data-dir' and `doom-cache-dir'. Must end with a slash.")
 
 (defvar doom-data-dir
-  (if doom-profile
-      (if doom--system-windows-p
-          (expand-file-name "doomemacs/data/" (getenv-internal "LOCALAPPDATA"))
-        (expand-file-name "doom/" (or (getenv-internal "XDG_DATA_HOME") "~/.local/share")))
-    ;; DEPRECATED: .local will be removed entirely in 3.0
-    (file-name-concat doom-local-dir "etc/"))
+  (if doom--noprofile
+      ;; DEPRECATED: .local will be removed entirely in 3.0
+      (file-name-concat doom-local-dir "etc/")
+    (if doom--system-windows-p
+        (expand-file-name "doomemacs/data/" (getenv-internal "LOCALAPPDATA"))
+      (expand-file-name "doom/" (or (getenv-internal "XDG_DATA_HOME") "~/.local/share"))))
   "Where Doom stores its global data files.
 
 Data files contain shared and long-lived data that Doom, Emacs, and their
@@ -246,12 +249,12 @@ generated files for profiles, profiles themselves, autoloads/loaddefs, etc.
 For profile-local data files, use `doom-profile-data-dir' instead.")
 
 (defvar doom-cache-dir
-  (if doom-profile
-      (if doom--system-windows-p
-          (expand-file-name "doomemacs/cache/" (getenv-internal "LOCALAPPDATA"))
-        (expand-file-name "doom/" (or (getenv-internal "XDG_CACHE_HOME") "~/.cache")))
-    ;; DEPRECATED: .local will be removed entirely in 3.0
-    (file-name-concat doom-local-dir "cache/"))
+  (if doom--noprofile
+      ;; DEPRECATED: .local will be removed entirely in 3.0
+      (file-name-concat doom-local-dir "cache/")
+    (if doom--system-windows-p
+        (expand-file-name "doomemacs/cache/" (getenv-internal "LOCALAPPDATA"))
+      (expand-file-name "doom/" (or (getenv-internal "XDG_CACHE_HOME") "~/.cache"))))
   "Where Doom stores its global cache files.
 
 Cache files represent unessential data that shouldn't be problematic when
@@ -265,12 +268,12 @@ session files, ELPA archives, authinfo files, org-persist, etc.
 For profile-local cache files, use `doom-profile-cache-dir' instead.")
 
 (defvar doom-state-dir
-  (if doom-profile
-      (if doom--system-windows-p
-          (expand-file-name "doomemacs/state/" (getenv-internal "LOCALAPPDATA"))
-        (expand-file-name "doom/" (or (getenv-internal "XDG_STATE_HOME") "~/.local/state")))
-    ;; DEPRECATED: .local will be removed entirely in 3.0
-    (file-name-concat doom-local-dir "state/"))
+  (if doom--noprofile
+      ;; DEPRECATED: .local will be removed entirely in 3.0
+      (file-name-concat doom-local-dir "state/")
+    (if doom--system-windows-p
+        (expand-file-name "doomemacs/state/" (getenv-internal "LOCALAPPDATA"))
+      (expand-file-name "doom/" (or (getenv-internal "XDG_STATE_HOME") "~/.local/state"))))
   "Where Doom stores its global state files.
 
 State files contain unessential, non-portable, but persistent data which, if
@@ -285,27 +288,34 @@ projects, recent files, bookmarks.
 For profile-local state files, use `doom-profile-state-dir' instead.")
 
 ;;; Profile file/directory variables
+;;; DEPRECATED: To be replaced with `doom-profile-cache-dir' function in v3
 (defvar doom-profile-cache-dir
-  (file-name-concat doom-cache-dir (car doom-profile))
+  (file-name-concat
+   doom-cache-dir (unless doom--noprofile (car doom-profile)))
   "For profile-local cache files under `doom-cache-dir'.")
 
+;;; DEPRECATED: To be replaced with `doom-profile-data-dir' function in v3
 (defvar doom-profile-data-dir
-  (file-name-concat doom-data-dir (car doom-profile))
+  (file-name-concat
+   doom-data-dir (unless doom--noprofile (car doom-profile)))
   "For profile-local data files under `doom-data-dir'.")
 
+;;; DEPRECATED: To be replaced with `doom-profile-state-dir' function in v3
 (defvar doom-profile-state-dir
-  (file-name-concat doom-state-dir (car doom-profile))
+  (file-name-concat
+   doom-state-dir (unless doom--noprofile (car doom-profile)))
   "For profile-local state files under `doom-state-dir'.")
 
+;;; DEPRECATED: To be replaced with `doom-profile-dir' function in v3
 (defconst doom-profile-dir
-  (file-name-concat doom-profile-data-dir "@" (cdr doom-profile))
+  (file-name-concat doom-profile-data-dir "@" (unless doom--noprofile (cdr doom-profile)))
   "Where generated files for the active profile (for Doom's core) are kept.")
 
 ;; DEPRECATED: Will be moved to cli/env
 (defconst doom-env-file
-  (file-name-concat (if doom-profile
-                        doom-profile-dir
-                      doom-local-dir)
+  (file-name-concat (if doom--noprofile
+                        doom-local-dir
+                      doom-profile-dir)
                     "env.el")
   "The location of your envvar file, generated by `doom env`.
 
@@ -393,18 +403,18 @@ appropriately against `noninteractive' or the `cli' context."
     ;;   build paths for storage/cache files instead of correctly using
     ;;   `locate-user-emacs-file'. Changing `user-emacs-directory' saves us the
     ;;   trouble of setting a million directory/file variables.
-    (setq user-emacs-directory doom-profile-cache-dir)
+    (setq user-emacs-directory (doom-profile-cache-dir t))
     ;; ...However, this can surprise packages (and users) that read
     ;; `user-emacs-directory' expecting to find the location of your Emacs
     ;; config, such as server.el!
-    (setq server-auth-dir (file-name-concat doom-emacs-dir "server/"))
+    (setq server-auth-dir (doom-emacs-dir "server/"))
 
     ;; Native compilation support (see http://akrl.sdf.org/gccemacs.html)
     (when (boundp 'native-comp-eln-load-path)
       ;; Don't store eln files in ~/.emacs.d/eln-cache (where they can easily be
       ;; deleted by 'doom upgrade').
       (setq native-comp-eln-load-path
-            (cons (expand-file-name "eln/" doom-profile-cache-dir)
+            (cons (doom-profile-cache-dir t "eln/")
                   (cdr native-comp-eln-load-path)))
 
       (unless (boundp 'native-comp-deferred-compilation-deny-list)
@@ -414,7 +424,7 @@ appropriately against `noninteractive' or the `cli' context."
         "Normally, native-comp writes a ton to /tmp. This advice redirects this
 IO to `doom-profile-cache-dir' instead, so it doesn't OOM tmpfs users and can be
 safely cleaned up with \\='doom sync' or \\='doom gc'."
-        (let ((temporary-file-directory (expand-file-name "comp/" doom-profile-cache-dir)))
+        (let ((temporary-file-directory (doom-profile-cache-dir t "comp/")))
           (make-directory temporary-file-directory t)
           (apply fn args)))
       ;; This is renamed in newer versions of Emacs.

lisp/lib/modules.el
@@ -35,7 +35,7 @@
                       :depth -115)
     ;; Load $DOOMDIR/init.el, where the user's `doom!' lives, which will inform
     ;; us of all desired modules.
-    (doom-load (file-name-concat doom-user-dir doom-module-init-file)
+    (doom-load (doom-user-dir doom-module-init-file)
                'noerror)))
 
 (cl-defun doom-module--put ((group . name) &rest plist)

lisp/lib/profiles.el
@@ -13,12 +13,13 @@ example: '~/.local/share/doom/_/@/0/'")
   (append
    (when-let* ((path (getenv-internal "DOOMPROFILELOADPATH")))
      (mapcar #'doom-path (split-string-and-unquote path path-separator)))
-   (list (file-name-concat doom-user-dir "profiles.el")
-         (file-name-concat doom-emacs-dir "profiles.el")
-         (expand-file-name "doom-profiles.el" (or (getenv "XDG_CONFIG_HOME") "~/.config"))
+   (list (doom-user-dir "profiles.el")
+         (expand-file-name
+          "doom-profiles.el" (or (getenv "XDG_CONFIG_HOME") "~/.config"))
          (expand-file-name "~/.doom-profiles.el")
-         (file-name-concat doom-user-dir "profiles")
-         (file-name-concat doom-emacs-dir "profiles")))
+         (doom-emacs-dir "profiles.el")
+         (doom-user-dir "profiles")
+         (doom-emacs-dir "profiles")))
   "A list of profile config files or directories that house implicit profiles.
 
 `doom-profiles-initialize' loads and merges all profiles defined in the above
@@ -43,8 +44,7 @@ list of paths or profile config files (semi-colon delimited on Windows).")
 Can be changed externally by setting $DOOMPROFILELOADFILE.")
 
 (defvar doom-profile-cache-file
-  (file-name-concat
-   doom-cache-dir (format "profiles.%s.el" (or (car doom-profile) "@")))
+  (doom-cache-dir (format "profiles.%s.el" (car doom-profile)))
   "Where Doom writes its interactive profile loader script.
 
 Can be changed externally by setting $DOOMPROFILELOADFILE.")
@@ -289,7 +289,7 @@ caches them in `doom--profiles'. If RELOAD? is non-nil, refresh the cache."
                       (doom-load doom-env-file t))
                    (current-buffer))
             (prin1 `(with-doom-context '(module init)
-                      (doom-load (file-name-concat doom-user-dir ,doom-module-init-file) t))
+                      (doom-load (doom-user-dir ,doom-module-init-file) t))
                    (current-buffer))
             (dolist (file (doom-glob init-dir "*.el"))
               (print-group! :level 'info
@@ -338,7 +338,7 @@ caches them in `doom--profiles'. If RELOAD? is non-nil, refresh the cache."
                  unless (and site-run-dir (file-in-directory-p path site-run-dir))
                  collect `(add-to-list 'load-path ,path))
       ,@(cl-loop with v = (version-to-list doom-version)
-                 with emacs-dir = (doom-path doom-emacs-dir)
+                 with emacs-dir = (doom-emacs-dir)
                  with ref = (doom-call-process "git" "-C" emacs-dir "rev-parse" "HEAD")
                  with branch = (doom-call-process "git" "-C" emacs-dir "branch" "--show-current")
                  for (var . val)
@@ -372,8 +372,7 @@ caches them in `doom--profiles'. If RELOAD? is non-nil, refresh the cache."
                 `(with-doom-module ',key
                    ,(if (equal key '(:user . nil))
                         `(doom-load
-                          (file-name-concat
-                           doom-user-dir ,(file-name-nondirectory noextfile))
+                          (doom-user-dir ,(file-name-nondirectory noextfile))
                           t)
                       (when (doom-file-cookie-p file "if" t)
                         `(doom-load ,(abbreviate-file-name noextfile) t))))))

commit e3f0ec1e75ef7cb9597de4962be1c7a51a632027

fix(cli): don't set DOOMPROFILE if it wasn't already
A regression introduced in ff0a836 which causes "Profiles not
initialized yet" errors whenever Doom restarts its CLI (e.g. when
running `doom upgrade`, or `doom sync` with the `:config literate`
module enabled).

Amend: ff0a83665321

lisp/doom-cli.el
@@ -1172,7 +1172,7 @@ Emacs' batch library lacks an implementation of the exec system call."
          (persisted-env
           (cl-remove-if-not
            #'cdr (append
-                  `(("DOOMPROFILE" . ,(doom-profile->id doom-profile))
+                  `(("DOOMPROFILE" . ,(if (getenv "DOOMPROFILE") (doom-profile->id doom-profile)))
                     ("EMACSDIR" . ,doom-emacs-dir)
                     ("DOOMDIR" . ,doom-user-dir)
                     ("DEBUG" . ,(if doom-debug-mode (number-to-string doom-log-level)))