simonfxr/doomemacs

commit ef93cba3ab6ca60c9f0b195dd499f127603c503b

refactor(lib): deprecate doom-module-load-path function

lisp/doom-lib.el
@@ -1622,13 +1622,15 @@ If ENABLED-ONLY?, return nil if the containing module isn't enabled."
             ((file-in-directory-p path doom-user-dir)
              (cons :user nil))))))
 
+;; DEPRECATED: Remove in v3
 (defun doom-module-load-path (&optional module-load-path)
   "Return a list of file paths to activated modules.
 
 The list is in no particular order and its file paths are absolute. If
 MODULE-DIRS is non-nil, include all modules (even disabled ones) available in
 those directories."
-  (declare (pure t) (side-effect-free t))
+  (declare (side-effect-free t)
+           (obsolete "Use `doom-module-get' in a loop instead" "2.2.0"))
   (mapcar #'doom-module-locate-path
           (doom-module-list (or module-load-path doom-module-load-path))))
 

lisp/lib/profiles.el
@@ -414,8 +414,10 @@ caches them in `doom--profiles'. If RELOAD? is non-nil, refresh the cache."
         ,@(doom-autoloads--scan
            (append (doom-glob doom-core-dir "lib/*.el")
                    (cl-loop for dir
-                            in (append (doom-module-load-path)
-                                       (list doom-user-dir))
+                            in (append
+                                (mapcan (doom-rpartial #'doom-glob "*/*/")
+                                        doom-module-load-path)
+                                (list doom-user-dir))
                             if (doom-glob dir "autoload.el") collect (car it)
                             if (doom-glob dir "autoload/*.el") append it)
                    (mapcan #'doom-glob doom-autoloads-files))

commit 3e83eb74609433257c478d39bc838714e64c4904

fix: don't index autoloads for shadowed modules
ef93cba3 introduced a regression: if :lang foo is enabled, then the
following autoloads files will all be indexed:

- $DOOMDIR/modules/lang/foo/autoload.el
- $EMACSDIR/modules/lang/foo/autoload.el
- $EMACSDIR/sources/doom+/modules/lang/foo/autoload.el

It should only index the autoloads file of the enabled module (i.e. in
the first module's root directory), which this commit fixes.

Revert: ef93cba3ab6c

lisp/doom-lib.el
@@ -1604,16 +1604,14 @@ If ENABLED-ONLY?, return nil if the containing module isn't enabled."
              (cons :user nil))))))
 
 ;; DEPRECATED: Remove in v3
-(defun doom-module-load-path (&optional module-load-path)
+(defun doom-module-load-path (&optional module-load-path initorder?)
   "Return a list of file paths to activated modules.
 
-The list is in no particular order and its file paths are absolute. If
-MODULE-DIRS is non-nil, include all modules (even disabled ones) available in
-those directories."
-  (declare (side-effect-free t)
-           (obsolete "Use `doom-module-get' in a loop instead" "2.2.0"))
-  (mapcar #'doom-module-locate-path
-          (doom-module-list (or module-load-path doom-module-load-path))))
+The list is in no particular order and its file paths are absolute.
+MODULE-LOAD-PATH can be a list of module tree root directories or `t' (return
+all modules in all known sources, collapsed by precedence)."
+  (declare (side-effect-free t))
+  (mapcar #'doom-module-locate-path (doom-module-list module-load-path initorder?)))
 
 (put :if     'lisp-indent-function 2)
 (put :when   'lisp-indent-function 'defun)

lisp/doom-profiles.el
@@ -414,10 +414,8 @@ caches them in `doom--profiles'. If RELOAD? is non-nil, refresh the cache."
         ,@(doom-autoloads--scan
            (append (doom-glob doom-core-dir "lib/*.el")
                    (cl-loop for dir
-                            in (append
-                                (mapcan (doom-rpartial #'doom-glob "*/*/")
-                                        doom-module-load-path)
-                                (list doom-user-dir))
+                            in (append (doom-module-load-path :all)
+                                       (list doom-user-dir))
                             if (doom-glob dir "autoload.el") collect (car it)
                             if (doom-glob dir "autoload/*.el") append it)
                    (mapcan #'doom-glob doom-autoloads-files))