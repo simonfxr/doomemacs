simonfxr/doomemacs

commit 32f31d2ba626f9fa6de2729a22d8889df40d98ad

fix: doom-package-list: not reading :user packages
A regression introduced in 177e775 causes `(doom-module-locate-path
'(:user))` to return `nil`, which prevents `doom-package-list` from
seeing user package overrides in $DOOMDIR/packages.el (e.g. `unpin!` or
`(package! ... :disable t)`).

First, this changes `doom-module-locate-path` to treat :user as a
special case (and it is, and always be, Doom's sole special case
module). And second, it fixes `doom-package-list`s use case,
which *should've* been using `doom-module-locate-path` as a fallback for
`doom-module-expand-path`.

Amend: 177e77593f4e

lisp/doom-modules.el
@@ -331,19 +331,21 @@ NAME is a symbol (e.g. \\='python). FILE is a string that will be appended to
 the resulting path. If said path doesn't exist, this returns nil, otherwise an
 absolute path."
     (let (file-name-handler-alist)
-      (cl-destructuring-bind (group . module) (doom-module-key key)
-        (when-let*
-            ((default-directory
-              (cl-loop with group = (doom-keyword-name group)
-                       with module = (if module (symbol-name module))
-                       with dir = (file-name-concat group module)
-                       for default-directory in doom-module-load-path
-                       if (file-directory-p dir)
-                       return (expand-file-name dir))))
-          (if file
-              (when (file-exists-p file)
-                (expand-file-name file))
-            default-directory)))))
+      (if (equal key '(:user))
+          (doom-module-expand-path key file)
+        (cl-destructuring-bind (group . module) (doom-module-key key)
+          (when-let*
+              ((default-directory
+                (cl-loop with group = (doom-keyword-name group)
+                         with module = (if module (symbol-name module))
+                         with dir = (file-name-concat group module)
+                         for default-directory in doom-module-load-path
+                         if (file-directory-p dir)
+                         return (expand-file-name dir))))
+            (if file
+                (when (file-exists-p file)
+                  (expand-file-name file))
+              default-directory))))))
 
   (defun doom-module-locate-paths (module-list file)
     "Return all existing paths to FILE under each module in MODULE-LIST.

lisp/doom-packages.el
@@ -381,8 +381,10 @@ keys."
         doom-packages)
     (letf! (defun read-packages (key)
              (with-doom-module key
-               (when-let* ((file (doom-module-locate-path
-                                  key doom-module-packages-file)))
+               (when-let* ((file (or (doom-module-expand-path
+                                      key doom-module-packages-file)
+                                     (doom-module-locate-path
+                                      key doom-module-packages-file))))
                  (doom-packages--read file nil 'noerror))))
       (with-doom-context 'package
         (let ((user? (assq :user module-list)))

commit 46687e54eebb05f1477686fb996f1a8172b17f29

fix: doom-module-locate-path: file-missing case & non-cons key
Regression introduced in 32f31d2, where `doom-module-locate-path` would
fail in two scenarios:

- If KEY is a `doom-module` or `doom-module-context` object.
- If KEY is '(:user), it would return its path (or it + FILE), even if
  either path didn't exist on the filesystem, causing read errors
  wherever the user relied on its null-if-file-doesn't-exist trait, e.g.
  calling any doom command without a $DOOMDIR/cli.el file would throw a
  file-missing error).

Amend: 32f31d2ba626

lisp/doom-modules.el
@@ -331,21 +331,21 @@ NAME is a symbol (e.g. \\='python). FILE is a string that will be appended to
 the resulting path. If said path doesn't exist, this returns nil, otherwise an
 absolute path."
     (let (file-name-handler-alist)
-      (if (equal key '(:user))
-          (doom-module-expand-path key file)
-        (cl-destructuring-bind (group . module) (doom-module-key key)
-          (when-let*
-              ((default-directory
+      (cl-destructuring-bind (group . module) (doom-module-key key)
+        (when-let*
+            ((default-directory
+              (if (equal (cons group module) '(:user))
+                  (doom-module-expand-path key)
                 (cl-loop with group = (doom-keyword-name group)
                          with module = (if module (symbol-name module))
                          with dir = (file-name-concat group module)
                          for default-directory in doom-module-load-path
                          if (file-directory-p dir)
-                         return (expand-file-name dir))))
-            (if file
-                (when (file-exists-p file)
-                  (expand-file-name file))
-              default-directory))))))
+                         return (expand-file-name dir)))))
+          (if file
+              (when (file-exists-p file)
+                (expand-file-name file))
+            default-directory)))))
 
   (defun doom-module-locate-paths (module-list file)
     "Return all existing paths to FILE under each module in MODULE-LIST.