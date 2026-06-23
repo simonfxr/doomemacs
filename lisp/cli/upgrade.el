b0c9aa3c91c34a5c68986e2ff46b2d2b887b20cd
Henrik Lissner <git@henrik.io>
2026-06-23 15:48:38 -0400

    fix(cli): doom upgrade: delete *.elc before restarting
    In case users are byte-compiling their own config, which can leave
    Doom's CLI in a broken state if done incorrectly (e.g. with
    `compile-angel`).

--- lisp/cli/upgrade.el
+++ lisp/cli/upgrade.el
@@ -65,6 +65,19 @@ libraries. It is the equivalent of the following shell commands:
       ;; Major changes will still break, however
       (print! (item "Reloading Doom Emacs"))
       (doom-cli-context-put context 'upgrading t)
+
+      ;; If users byte-compile Doom (or misconfigure `compile-angel'), this can
+      ;; leave Doom in a broken state post-upgrade, and cause other odd errors
+      ;; in interactive sessions.
+      (when-let*
+          ((elc (doom-files-in (cons doom-core-dir doom-module-load-path)
+                               :follow-symlinks nil
+                               :match "\\.elc\\$")))
+        (print! (start "Cleaning up byte-compiled files in Doom core..."))
+        (dolist (file elc)
+          (print! (item "Deleted %s") (path file))
+          (ignore-errors (delete-file file))))
+
       (exit! "doom" "upgrade" "-p"
              (if aot? "--aot")
              (if nobuild? "-B")
