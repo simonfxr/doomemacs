simonfxr/doomemacs

commit 2014f96c46a02e7feec9b57137b926a18c68eb8d

feat(lib): new loaddefs.el library
To replace the autoloads.el lib, which is now deprecated (and
appropriately aliased).

This isn't a very big change, I'm merely backporting architectural
changes from v3 to simplify rebasing.

lisp/doom-profiles.el
@@ -450,22 +450,20 @@ caches them in `doom--profiles'. If RELOAD? is non-nil, refresh the cache."
 (defun doom-profile--generate-loaddefs-doom (_profile)
   (doom-file-write
    "10-doom-loaddefs.init.el"
-   (doom-autoloads--scan (doom-glob doom-core-dir "doom-*.el")
-                         nil))
+   (doom-loaddefs-scan (doom-glob doom-core-dir "doom-*.el")))
   (doom-file-write
    "10-doom-cli-loaddefs.load.el"
    `(";; -*- lexical-binding: t; no-byte-compile t; -*-"
      ";;;###if noninteractive"
-     ,@(doom-autoloads--scan
-        (append (cl-loop for dir in (doom-module-load-path nil t)
-                         append (doom-glob dir doom-module-cli-file))
-                (doom-glob doom-core-dir "cli/*.el")
-                (seq-filter
-                 #'doom-cli-executable-p
-                 (cl-loop for dir in doom-cli-load-path
-                          append (doom-glob dir "doom-*")
-                          append (doom-glob dir "doom-*.el"))))
-        nil))))
+     ,@(doom-loaddefs-scan
+        (cl-loop for dir in (doom-module-load-path nil t)
+                 append (doom-glob dir doom-module-cli-file))
+        (doom-glob doom-core-dir "cli/*.el")
+        (seq-filter
+         #'doom-cli-executable-p
+         (cl-loop for dir in doom-cli-load-path
+                  append (doom-glob dir "doom-*")
+                  append (doom-glob dir "doom-*.el")))))))
 
 (defun doom-profile--generate-user-init-loader (_profile)
   (doom-file-write
@@ -491,21 +489,20 @@ caches them in `doom--profiles'. If RELOAD? is non-nil, refresh the cache."
   (doom-file-write
    "60-doom-module-loaddefs.init.el"
    `((defun doom--startup-loaddefs-modules (_profile)
-       ,@(doom-autoloads--scan
-          (append (doom-glob doom-core-dir "lib/*.el")
-                  (cl-loop for dir
-                           in (append (doom-module-load-path :all t)
-                                      (list doom-user-dir))
-                           if (doom-glob dir "autoload.el") collect (car it)
-                           if (doom-glob dir "autoload/*.el") append it))
-          nil))
+       ,@(doom-loaddefs-scan
+          (doom-glob doom-core-dir "lib/*.el")
+          (cl-loop for dir
+                   in (append (doom-module-load-path :all t)
+                              (list doom-user-dir))
+                   if (doom-glob dir "autoload.el") collect (car it)
+                   if (doom-glob dir "autoload/*.el") append it)))
      (add-hook 'doom-startup-functions #'doom--startup-loaddefs-modules 60))))
 
 (defun doom-profile--generate-loaddefs-packages (_profile)
   (doom-file-write
    "70-doom-package-loaddefs.init.el"
    `((defun doom--startup-loaddefs-packages (_profile)
-       ,@(doom-autoloads--scan
+       ,@(doom-loaddefs-scan-literal
           ;; Create a list of packages starting with the Nth-most dependencies
           ;; by walking the package dependency tree depth-first. This ensures
           ;; any load-order constraints in package autoloads are always met.
@@ -518,9 +515,7 @@ caches them in `doom--profiles'. If RELOAD? is non-nil, refresh the cache."
                            ((listp pkglist)
                             (mapc #'walk-packages (reverse pkglist)))))
               (walk-packages (mapcar #'symbol-name (mapcar #'car doom-packages))))
-            (mapcar #'straight--autoloads-file (nreverse packages)))
-          doom-autoloads-excluded-files
-          'literal)
+            (mapcar #'straight--autoloads-file (nreverse packages))))
        ,@(when-let* ((info-dirs
                       (cl-loop for dir in load-path
                                if (file-exists-p (doom-path dir "dir"))

lisp/lib/loaddefs.el
@@ -1,0 +1,204 @@
+;;; lisp/lib/loaddefs.el -*- lexical-binding: t; -*-
+;;; Commentary:
+;;
+;; Doom's spin on loaddefs.el, which isn't extensible enough for us to build on
+;; top of. This library, on top of regular ;;;###autoload cookies, adds support
+;; for:
+;;
+;;   - Autoloading `defcli!'s (converting them in to `defcli-autoload!' calls)
+;;   - Reading ;;;###autodef cookies (autoloaded functions/macros that are
+;;     always defined, even if their containing module is disabled — they're
+;;     replaced with a no-op).
+;;   - Cleaning up extra, unneeded work done in autoloads files.
+;;   - Expanding paths in package autoloads to reduce load-path lookups.
+;;
+;;; Code:
+
+(defvar doom-loaddefs-excluded-vars
+  '(load-path
+    auto-mode-alist
+    interpreter-mode-alist
+    magic-mode-alist
+    magic-fallback-mode-alist)
+  "A list of variables to remove `add-to-list' calls for in autoloads.
+
+These are removed because Doom's profiles batch these calls in their init
+file.")
+
+(defvar doom-loaddefs--path-cache nil)
+
+
+;;
+;;; * Helpers
+
+(defun doom-loaddefs--locate-file (path)
+  (or (cdr (assoc path doom-loaddefs--path-cache))
+      (when-let* ((libpath (locate-library path))
+                  (libpath (file-name-sans-extension libpath))
+                  (libpath (abbreviate-file-name libpath)))
+        (push (cons path libpath) doom-loaddefs--path-cache)
+        libpath)
+      (and (stringp path) (abbreviate-file-name path))
+      path))
+
+(defun doom-loaddefs--clean (file form &optional expand-autoloads?)
+  (let ((f (car-safe form)))
+    (cond ((memq f '(provide custom-autoload register-definition-prefixes))
+           nil)
+
+          ((and (eq f 'add-to-list)
+                (memq (doom-unquote (cadr form))
+                      doom-loaddefs-excluded-vars))
+           nil)
+
+          ((eq f 'defcli!)
+           (setcar form 'defcli-autoload!)
+           (setcdr form (list (cadr form) (doom-loaddefs--locate-file file)))
+           form)
+
+          ((and (eq f 'autoload)
+                expand-autoloads?
+                (not (file-name-absolute-p file)))
+           (setf (nth 2 form) (doom-loaddefs--locate-file (nth 2 form)))
+           form)
+
+          (form))))
+
+(defun doom-loaddefs--scan-autodefs (file buffer module &optional module-enabled-p)
+  (with-temp-buffer
+    (insert-file-contents file)
+    (while (re-search-forward "^;;;###autodef *\\([^\n]+\\)?\n" nil t)
+      (let* ((standard-output buffer)
+             (form    (read (current-buffer)))
+             (altform (match-string 1))
+             (definer (car-safe form))
+             (symbol  (doom-unquote (cadr form))))
+        (cond ((and (not module-enabled-p) altform)
+               (print (read altform)))
+              ((memq definer '(defun defmacro cl-defun cl-defmacro))
+               (print
+                (if module-enabled-p
+                    (make-autoload form (abbreviate-file-name file))
+                  (seq-let (_ _ arglist &rest body) form
+                    (if altform
+                        (read altform)
+                      (append
+                       (list (pcase definer
+                               (`defun 'defmacro)
+                               (`cl-defun `cl-defmacro)
+                               (_ definer))
+                             symbol arglist
+                             (format "THIS FUNCTION DOES NOTHING BECAUSE %s IS DISABLED\n\n%s"
+                                     module (if (stringp (car body))
+                                                (pop body)
+                                              "No documentation.")))
+                       (cl-loop for arg in arglist
+                                if (symbolp arg)
+                                if (not (keywordp arg))
+                                if (not (memq arg cl--lambda-list-keywords))
+                                collect arg into syms
+                                else if (listp arg)
+                                collect (car arg) into syms
+                                finally return (if syms `((ignore ,@syms)))))))))
+               (print `(put ',symbol 'doom-module ',module)))
+              ((eq definer 'defalias)
+               (seq-let (_ _ target docstring) form
+                 (unless module-enabled-p
+                   (setq target #'ignore
+                         docstring
+                         (format "THIS FUNCTION DOES NOTHING BECAUSE %s IS DISABLED\n\n%s"
+                                 module docstring)))
+                 (print `(put ',symbol 'doom-module ',module))
+                 (print `(defalias ',symbol #',(doom-unquote target) ,docstring))))
+              (module-enabled-p (print form)))))))
+
+(defvar autoload-timestamps)
+(defvar generated-autoload-load-name)
+(defun doom-loaddefs--scan-file (file)
+  ;; FIXME: Conditionally use `loaddefs' in Emacs 30+
+  (quiet! (require 'autoload))  ; silence deprecation notice
+  (let* (;; Prevent `autoload-find-file' from firing file hooks, e.g. adding
+         ;; to recentf.
+         find-file-hook
+         write-file-functions
+         ;; Prevent a possible source of crashes when there's a syntax error in
+         ;; the autoloads file.
+         debug-on-error
+         ;; Non-nil interferes with autoload generation in Emacs < 29. See
+         ;; radian-software/straight.el#904.
+         (left-margin 0)
+         ;; The following bindings are in `package-generate-autoloads'.
+         ;; Presumably for a good reason, so I just copied them.
+         (backup-inhibited t)
+         (version-control 'never)
+         case-fold-search    ; reduce magic
+         autoload-timestamps ; reduce noise in generated files
+         autoload-compute-prefixes
+         ;; So `autoload-generate-file-autoloads' knows where to write it
+         (target-buffer (current-buffer))
+         (generated-autoload-load-name
+          (abbreviate-file-name (file-name-sans-extension file)))
+         (module (doom-module-from-path file))
+         (module-enabled-p
+          (and (doom-module-active-p (car module) (cdr module))
+               (doom-file-cookie-p file "if" t))))
+    (save-excursion
+      (when module-enabled-p
+        (quiet! (autoload-generate-file-autoloads file target-buffer)))
+      (doom-loaddefs--scan-autodefs
+       file target-buffer module module-enabled-p))))
+
+(defun doom-loaddefs--read (files thunk &optional literal?)
+  (let (seen forms)
+    (dolist (file (thread-last files
+                               (flatten-list)
+                               (remq nil)
+                               (mapcar #'file-truename)
+                               (delete-dups)))
+      (when (and (not (member file seen))
+                 (file-readable-p file))
+        (push file seen)
+        (doom-log "loaddefs:read: %s (%s)"
+                  (abbreviate-file-name file)
+                  (if (symbolp thunk) thunk "<lambda>"))
+        (with-temp-buffer
+          (let (subautoloads)
+            (funcall thunk file)
+            (save-excursion
+              ;; Fixup the special #$ reader form and throw away comments.
+              (while (re-search-forward "#\\$\\|^;\\(.*\n\\)" nil 'move)
+                (unless (ppss-string-terminator (save-match-data (syntax-ppss)))
+                  (replace-match (if (match-end 1) "" file) t t))))
+            (let ((load-file-name file)
+                  (load-true-file-name load-file-name)
+                  (load-path
+                   (append (list doom-user-dir)
+                           doom-module-load-path
+                           load-path)))
+              (condition-case _
+                  (while t
+                    (push (doom-loaddefs--clean
+                           file (read (current-buffer)) (not literal?))
+                          subautoloads))
+                (end-of-file)))
+            (when (delq nil subautoloads)
+              (push `(let* ((load-file-name ,(abbreviate-file-name file))
+                            (load-true-file-name load-file-name))
+                       ,@(nreverse subautoloads))
+                    forms))))))
+    `((let ((load-in-progress t)) ,@(nreverse (delq nil forms))))))
+
+
+;;
+;;; * Public functions
+
+(defun doom-loaddefs-scan (&rest files)
+  (doom-loaddefs--read
+   files #'doom-loaddefs--scan-file))
+
+(defun doom-loaddefs-scan-literal (&rest files)
+  (doom-loaddefs--read
+   files #'insert-file-contents t))
+
+(provide 'doom-lib '(loaddefs))
+;;; loaddefs.el end here

lisp/doom-cli.el
@@ -15,6 +15,7 @@
   (doom-require 'doom-lib 'system)
   (doom-require 'doom-lib 'files)
   (doom-require 'doom-lib 'print)
+  ;; TODO: (doom-require 'doom-lib 'loaddefs)
   (doom-require 'doom-lib 'autoloads))
 
 (require 'doom-profiles)

lisp/lib/autoloads.el
@@ -1,4 +1,10 @@
 ;;; lisp/lib/autoloads.el -*- lexical-binding: t; -*-
+;;; Commentary:
+;;
+;; DEPRECATED: Old autoload processing library, replaced by lib/loaddefs.el.
+;; Will be removed in v3.
+;;
+;;; Code:
 
 (defvar doom-autoloads-excluded-files ()
   "List of regexps whose matching files won't be indexed for autoloads.")
@@ -6,137 +12,24 @@
                         "Filter files before passing them to `doom-autoloads--scan'"
                         "2.3.0")
 
-(defvar doom-autoloads-cached-vars
-  '(load-path
-    auto-mode-alist
-    interpreter-mode-alist
-    magic-mode-alist
-    magic-fallback-mode-alist
-    Info-directory-list)
-  "A list of variables to be cached in `doom-autoloads-file'.")
+(define-obsolete-variable-alias 'doom-autoloads-cached-vars 'doom-loaddefs-excluded-vars "2.3.0")
 
 
 ;;
 ;;; * Library
 
-(defun doom-autoloads--cleanup-form (form &optional expand)
-  (let ((func (car-safe form)))
-    (cond ((memq func '(provide custom-autoload register-definition-prefixes))
-           nil)
-          ;; HACK: Remove modifications to `auto-mode-alist' and
-          ;;   `interpreter-mode-alist' in *-ts-mode package. They are applied
-          ;;   twice and often overwrite user or module configuration.
-          ((equal (list func (car (cdr-safe form))) '(when (treesit-available-p)))
-           (setf (nth 2 form)
-                 (cl-loop for form in (nth 2 form)
-                          if (or (not (eq (car-safe form) 'add-to-list))
-                                 (not (memq (nth 1 form) '(auto-mode-alist interpreter-mode-alist))))
-                          collect form))
-           form)
-          ((and (eq func 'add-to-list)
-                (memq (doom-unquote (cadr form))
-                      doom-autoloads-cached-vars))
-           nil)
-          ((not (eq func 'autoload))
-           form)
-          ((and expand (not (file-name-absolute-p (nth 2 form))))
-           (defvar doom--autoloads-path-cache nil)
-           (setf (nth 2 form)
-                 (let ((path (nth 2 form)))
-                   (or (cdr (assoc path doom--autoloads-path-cache))
-                       (when-let* ((libpath (locate-library path))
-                                   (libpath (file-name-sans-extension libpath))
-                                   (libpath (abbreviate-file-name libpath)))
-                         (push (cons path libpath) doom--autoloads-path-cache)
-                         libpath)
-                       path)))
-           form)
-          (form))))
+(doom-require 'doom-lib 'loaddefs)
 
-(defun doom-autoloads--scan-autodefs (file buffer module &optional module-enabled-p)
-  (with-temp-buffer
-    (insert-file-contents file)
-    (while (re-search-forward "^;;;###autodef *\\([^\n]+\\)?\n" nil t)
-      (let* ((standard-output buffer)
-             (form    (read (current-buffer)))
-             (altform (match-string 1))
-             (definer (car-safe form))
-             (symbol  (doom-unquote (cadr form))))
-        (cond ((and (not module-enabled-p) altform)
-               (print (read altform)))
-              ((memq definer '(defun defmacro cl-defun cl-defmacro))
-               (print
-                (if module-enabled-p
-                    (make-autoload form (abbreviate-file-name file))
-                  (seq-let (_ _ arglist &rest body) form
-                    (if altform
-                        (read altform)
-                      (append
-                       (list (pcase definer
-                               (`defun 'defmacro)
-                               (`cl-defun `cl-defmacro)
-                               (_ definer))
-                             symbol arglist
-                             (format "THIS FUNCTION DOES NOTHING BECAUSE %s IS DISABLED\n\n%s"
-                                     module (if (stringp (car body))
-                                                (pop body)
-                                              "No documentation.")))
-                       (cl-loop for arg in arglist
-                                if (symbolp arg)
-                                if (not (keywordp arg))
-                                if (not (memq arg cl--lambda-list-keywords))
-                                collect arg into syms
-                                else if (listp arg)
-                                collect (car arg) into syms
-                                finally return (if syms `((ignore ,@syms)))))))))
-               (print `(put ',symbol 'doom-module ',module)))
-              ((eq definer 'defalias)
-               (seq-let (_ _ target docstring) form
-                 (unless module-enabled-p
-                   (setq target #'ignore
-                         docstring
-                         (format "THIS FUNCTION DOES NOTHING BECAUSE %s IS DISABLED\n\n%s"
-                                 module docstring)))
-                 (print `(put ',symbol 'doom-module ',module))
-                 (print `(defalias ',symbol #',(doom-unquote target) ,docstring))))
-              (module-enabled-p (print form)))))))
+;; DEPRECATED: Remove in v3
+(define-obsolete-function-alias 'doom-autoloads--cleanup-form #'doom-loaddefs--clean "2.3.0")
 
-(defvar autoload-timestamps)
-(defvar generated-autoload-load-name)
-(defun doom-autoloads--scan-file (file)
-  (let* (;; Prevent `autoload-find-file' from firing file hooks, e.g. adding
-         ;; to recentf.
-         find-file-hook
-         write-file-functions
-         ;; Prevent a possible source of crashes when there's a syntax error in
-         ;; the autoloads file.
-         debug-on-error
-         ;; Non-nil interferes with autoload generation in Emacs < 29. See
-         ;; radian-software/straight.el#904.
-         (left-margin 0)
-         ;; The following bindings are in `package-generate-autoloads'.
-         ;; Presumably for a good reason, so I just copied them.
-         (backup-inhibited t)
-         (version-control 'never)
-         case-fold-search    ; reduce magic
-         autoload-timestamps ; reduce noise in generated files
-         autoload-compute-prefixes
-         ;; So `autoload-generate-file-autoloads' knows where to write it
-         (target-buffer (current-buffer))
-         (module (doom-module-from-path file))
-         (generated-autoload-load-name
-          (abbreviate-file-name (file-name-sans-extension file)))
-         (module-enabled-p
-          (and (doom-module-active-p (car module) (cdr module))
-               (doom-file-cookie-p file "if" t)))
-         ;; (load-path (append doom-module-load-path load-path))
-         )
-    (save-excursion
-      (when module-enabled-p
-        (quiet! (autoload-generate-file-autoloads file target-buffer)))
-      (doom-autoloads--scan-autodefs
-       file target-buffer module module-enabled-p))))
+;; DEPRECATED: Remove in v3
+(define-obsolete-function-alias 'doom-autoloads--scan-autodefs #'doom-loaddefs--scan-autodefs "2.3.0")
 
+;; DEPRECATED: Remove in v3
+(define-obsolete-function-alias 'doom-autoloads--scan-file #'doom-loaddefs--scan-file "2.3.0")
+
+;; DEPRECATED: Remove in v3 (replaced by lib/loaddefs.el)
 (defun doom-autoloads--scan (files &optional exclude literal)
   "Scan and return all autoloaded forms in FILES.
 
@@ -172,8 +65,9 @@ non-nil, treat FILES as pre-generated autoload files instead."
                            load-path)))
               (condition-case _
                   (while t
-                    (push (doom-autoloads--cleanup-form (read (current-buffer))
-                                                        (not literal))
+                    (push (doom-loaddefs--clean load-file-name
+                                                (read (current-buffer))
+                                                (not literal))
                           subautoloads))
                 (end-of-file)))
             (when (delq nil subautoloads)

commit cf29603ca58021df4566e9c275ceb55a2fed4375

fix(cli): unexpanded autoload paths
The fix employed in cc6c480 masked the real problem: a regression
introduced in 2014f96 causing relative paths in `autoload` forms not
getting expanded into absolute paths, causing Emacs to fail to locate
many autoloaded symbols (#8831).

Fix: #8831
Revert: cc6c480a8f5c
Amend: 2014f96c46a0

lisp/cli/loaddefs.el
@@ -58,7 +58,7 @@ file.")
 
           ((and (eq f 'autoload)
                 expand-autoloads?
-                (not (file-name-absolute-p file)))
+                (not (file-name-absolute-p (nth 2 form))))
            (setf (nth 2 form) (doom-loaddefs--locate-file (nth 2 form)))
            form)
 

lisp/doom-profiles.el
@@ -438,8 +438,6 @@ caches them in `doom--profiles'. If RELOAD? is non-nil, refresh the cache."
                   unless (file-in-directory-p path data-directory)
                   unless (and site-run-dir (file-in-directory-p path site-run-dir))
                   collect `(add-to-list 'load-path ,path))
-       ,@(cl-loop for dir in doom-module-load-path
-                  collect `(add-to-list 'load-path ,dir))
        ,@(cl-loop with v = (version-to-list doom-version)
                   with emacs-dir = (doom-emacs-dir)
                   with ref = (doom-call-process "git" "-C" emacs-dir "rev-parse" "HEAD")